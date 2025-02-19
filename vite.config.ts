f01ef7631948e4df63ab5fabca48386f71dd28b1
dhkim <dhkim@bizbee.co.kr>
2025-02-19 13:35:19 +0900

    core : 환경변수 프록시 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
+
+const apiBaseUrl = process.env.VITE_API_BASE_URL || 'http://localhost:9080';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // FCM 미들웨어 서버
+        target: apiBaseUrl, // ✅ env 파일 기반 설정
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 });
